3b9cb2561776a1142a0f0ecd8f1c557ef2a3c027
Arindam Karmakar <connect@karindam.in>
2025-06-07 20:21:43 +0530

    Project Created

---
+++ android/app/src/main/kotlin/in/karindam/nukiveedge/MainActivity.kt
@@ -0,0 +1,5 @@
+package `in`.karindam.nukiveedge
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+Future<void> main() async => runApp(const NukiveEdge());
+
+class NukiveEdge extends StatefulWidget {
+  const NukiveEdge({super.key});
+
+  @override
+  State<NukiveEdge> createState() => _NukiveEdgeState();
+}
+
+class _NukiveEdgeState extends State<NukiveEdge> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}

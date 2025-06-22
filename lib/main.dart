f1fba23ede93614e07e197aafe36109420664380
Arindam Karmakar <connect@karindam.in>
2025-06-23 02:48:54 +0530

    Project Re-created

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:nukiveedge/firebase_options.dart';
+import 'package:nukiveedge/src/nukive_edge_lite/app.dart';
+import 'package:nukiveedge/src/nukive_edge_pro/app.dart';
+import 'package:nukiveedge/src/unsupported_platform_entrypoint.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  final crashlytics = FirebaseCrashlytics.instance;
+
+  FlutterError.onError = crashlytics.recordFlutterFatalError;
+
+  PlatformDispatcher.instance.onError = (error, stack) {
+    crashlytics.recordError(error, stack, fatal: true);
+    return true;
+  };
+
+  final orientations = [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown];
+
+  SystemChrome.setPreferredOrientations(orientations).then(
+    (_) => runApp(platformApp()),
+  );
+}
+
+Widget platformApp() {
+  if (kIsWeb) return const NukiveUnsupported();
+
+  if (Platform.isIOS || Platform.isAndroid) return const NukiveEdgeLite();
+
+  if (Platform.isMacOS) return const NukiveEdgePro();
+
+  return const NukiveUnsupported();
+}
